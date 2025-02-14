dbbd275346faf8333229101a2469c8b1b2921f19
Kai Stonebanks <2842828S@student.gla.ac.uk>
2025-02-14 09:12:42 +0000

    Chapter 4 done

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.conf import settings
 
 def index(request):
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!', 'static_url': settings.STATIC_URL}
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    
-    return render(request, 'rango/about.html')
+    context_dict = {'media_url': settings.MEDIA_URL, 'static_url': settings.STATIC_URL}
+    return render(request, 'rango/about.html', context=context_dict)
