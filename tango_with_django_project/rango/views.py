17a3c60afa7be9fa390b09b710bfc49599e3837d
Kai Stonebanks <2842828S@student.gla.ac.uk>
2025-02-14 13:31:08 +0000

    Chapter 8 done

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -40,10 +40,8 @@ def add_category(request):
     if request.method == 'POST':
         form = CategoryForm(request.POST)
         if form.is_valid():
-            form.save(commit=True)
-            return redirect('/rango/')
-        else:
-            print(form.errors)
+            form.save()
+            return redirect(reverse('rango:index'))
     return render(request, 'rango/add_category.html', {'form': form})
 
 def add_page(request, category_name_slug):
