8bb69f6a89871b668b0b9b203e8f47868098cd57
Kai Stonebanks <2842828S@student.gla.ac.uk>
2026-02-13 12:10:33 +0000

    chap 7 done

--- tango_with_django_project/rango/forms.py
+++ tango_with_django_project/rango/forms.py
@@ -1,7 +1,6 @@
 from django import forms
 from rango.models import Page, Category
 from django.shortcuts import render, redirect
-from rango.forms import CategoryForm
 
 class CategoryForm(forms.ModelForm):
     name = forms.CharField(max_length=128)
@@ -21,6 +20,17 @@ class PageForm(forms.ModelForm):
     class Meta:
         model = Page
         exclude = ('category',)
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+    
+        if url and not url.startswith('http://'):
+            url = f'http://{url}'
+            cleaned_data['url'] = url
+
+        return cleaned_data
+
 
 def add_category(request):
     form = CategoryForm()
--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from rango.models import Category
 from rango.models import Page
+from rango.models import Category
+from rango.forms import CategoryForm, PageForm
+
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
@@ -19,8 +22,18 @@ def about(request):
     context_dict = {'cat_image': 'cat.jpg'}
     return render(request, 'rango/about.html', context=context_dict)
 
-from django.shortcuts import render
-from rango.models import Category, Page
+def add_category(request):
+    form = CategoryForm()
+
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            return redirect('rango:index')
+        else:
+            print(form.errors)
+
+    return render(request, 'rango/add_category.html', {'form': form})
 
 def show_category(request, category_name_slug):
     context_dict = {}
@@ -38,3 +51,24 @@ def show_category(request, category_name_slug):
         context_dict['pages'] = None
 
     return render(request, 'rango/category.html', context=context_dict)
+
+def add_page(request, category_name_slug):
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+        return redirect('rango:index')
+
+    form = PageForm()
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+        if form.is_valid():
+            page = form.save(commit=False)
+            page.category = category
+            page.views = form.cleaned_data.get('views', 0)
+            page.save()
+            return redirect('rango:show_category', category_name_slug=category.slug)
+        else:
+            print(form.errors)
+
+    return render(request, 'rango/add_page.html', {'form': form, 'category': category})
+
